e1c3c800fbc09c9ecdc18035e98e5598b21826df
Lucas Rosa <lucasmetron@gmail.com>
2024-08-02 16:03:37 -0300

    logica para gerar minas

---
+++ src/functions.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-shadow */
+
+function creatBoard(rows, columns) {
+  return Array(rows)
+    .fill(0)
+    .map((_, row) => {
+      return Array(columns)
+        .fill(0)
+        .map((_, column) => {
+          return {
+            row,
+            column,
+            opened: false,
+            flagged: false,
+            mined: false,
+            exploded: false,
+            nearMines: 0,
+          };
+        });
+    });
+}
+
+function spreadMines(board, minesAmount) {
+  const rows = board.length;
+  const columns = board[0].length;
+  let minesPlanted = 0;
+
+  while (minesPlanted < minesAmount) {
+    const rowSel = parseInt(Math.random() * rows, 10);
+    const columnSel = parseInt(Math.random() * columns, 10);
+
+    if (!board[rowSel][columnSel].mined) {
+      board[rowSel][columnSel].mined = true;
+      minesPlanted++;
+    }
+  }
+}
+
+export function createMineBoard(rows, columns, minesAmount) {
+  const board = creatBoard(rows, columns);
+  spreadMines(board, minesAmount);
+  return board;
+}
