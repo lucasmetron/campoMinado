ea8de47a7c85942b44eefc89855f48cd807c0884
Lucas Rosa <lucasmetron@gmail.com>
2024-08-02 13:42:06 -0300

    flag

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
-const Flag = () => {
+const Flag = ({bigger}) => {
   return (
     <View style={styles.container}>
-      <View style={styles.flagPole} />
-      <View style={styles.flag} />
-      <View style={styles.base1} />
-      <View style={styles.base2} />
+      <View style={[styles.flagPole, bigger ? styles.flagPoleBigger : null]} />
+      <View style={[styles.flag, bigger ? styles.flagBigger : null]} />
+      <View style={[styles.base1, bigger ? styles.base1Bigger : null]} />
+      <View style={[styles.base2, bigger ? styles.base2Bigger : null]} />
     </View>
   );
 };
@@ -45,6 +45,28 @@ const styles = StyleSheet.create({
     height: 2,
     marginTop: 12,
     marginLeft: 5,
+  },
+  flagPoleBigger: {
+    height: 28,
+    width: 4,
+    marginLeft: 16,
+  },
+  flagBigger: {
+    height: 10,
+    width: 14,
+    marginLeft: 3,
+  },
+  base1Bigger: {
+    width: 12,
+    height: 4,
+    marginTop: 20,
+    marginLeft: 12,
+  },
+  base2Bigger: {
+    width: 20,
+    height: 4,
+    marginTop: 24,
+    marginLeft: 8,
   },
 });
 
